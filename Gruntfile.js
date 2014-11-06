f3593aabdbc92b9ba442346b88e9d158ebfcc1fc
makakoa <makakoa15@gmail.com>
2014-11-05 16:46:45 -0800

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-jscs');
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+	   node: true
+      },
+      src: ['models/**/*.js', 'server.js', 'routes/**/*.js']
+    },
+
+    simplemocha: {
+      src: ['test/api/**/*.js']
+    },
+
+    jscs: {
+      src: ['model/**/*.js', 'server.js', 'routes/**/*.js'],
+      options: {
+	   config: '.jscsrc'
+      }
+    }
+  });
+
+  grunt.registerTask('test', ['jshint', 'simplemocha', 'jscs']);
+};
