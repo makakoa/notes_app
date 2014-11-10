e97f732195dd884b88afb5e4b241dd7d52b66c67
makakoa <makakoa15@gmail.com>
2014-11-09 20:00:20 -0800

    fixed note and routes

--- routes/notes_routes.js
+++ routes/notes_routes.js
@@ -6,13 +6,13 @@ var Note = require('../models/note');
 module.exports = function(app) {
   app.get('/api/notes', function(req, res) {
     Note.find({}, function(err, data) {
-      if (err) return res.send(err);
-      res.send(data);
+      if (err) return res.status(500).send('error');
+      res.json(data);
     });
   });
 
   app.get('/api/notes/:id', function(req, res) {
-    Note.findOne({_id: req.params.id}, function(err, data) {
+    Note.findOne({'_id': req.params.id}, function(err, data) {
       if (err) return res.status(500).send('error');
       res.json(data);
     });
@@ -29,14 +29,14 @@ module.exports = function(app) {
   app.put('/api/notes/:id', function(req, res) {
     var note = req.body;
     delete note._id;
-    Note.findOneAndUpdate({_id: req.params.id}, note, function(err, data) {
+    Note.findOneAndUpdate({'_id': req.params.id}, note, function(err, data) {
       if (err) return res.status(500).send('error');
       res.json(data);
     });
   });
 
   app.delete('/api/notes/:id', function(req, res)  {
-    Note.remove({_id:req.params.id}, function(err) {
+    Note.remove({'_id':req.params.id}, function(err) {
       if (err) return res.status(500).send('error');
       res.json({msg: 'success!'});
     });
