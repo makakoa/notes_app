01530e79ddca4abd7a2977d4e6faa30d15ef539c
makakoa <makakoa15@gmail.com>
2014-11-10 16:19:08 -0800

    added auth auth

---
+++ routes/user_routes.js
@@ -0,0 +1,27 @@
+/*jshint node: true*/
+'use strict';
+
+var User = require('../models/user');
+
+module.exports = function(app, passport) {
+  app.get('/api/users', passport.authenticate('basic', {session: false}), function(req, res) {
+    res.json({'jwt': req.user.generateToken(app.get('jwtSecret'))});
+  });
+
+  app.post('/api/users', function(req ,res) {
+    User.findOne({'basic.email': req.body.email}, function(err, user) {
+      if (err) return res.status(500).send('server error');
+      if (user) return res.status(500).send('cannot create that user');
+
+      // put in a check password and validation
+
+      var newUser = new User();
+      newUser.basic.email = req.body.email;
+      newUser.basic.password = newUser.generateHash(req.body.password);
+      newUser.save(function(err, data) {
+        if (err) return res.status(500).send('server error');
+        res.json({'jwt': newUser.generateToken(app.get('jwtSecret'))});
+      });
+    });
+  });
+};
