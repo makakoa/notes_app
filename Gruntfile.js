dd4464fe85b339a4003be097b759a1ce4b962c0e
makakoa <makakoa15@gmail.com>
2014-12-01 09:41:27 -0800

    added browserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-browserify');
 
   grunt.initConfig({
     jshint: {
@@ -21,9 +24,44 @@ module.exports = function(grunt) {
       options: {
           config: '.jscsrc'
       }
+    },
+
+    clean: {
+      dev: {
+        src: ['build/']
+      }
+    },
+
+    copy: {
+      dev: {
+        cwd: 'app/',
+        src: ['**/*.html', 'css/**/*.css'],
+        expand: true,
+        dest: 'build/'
+      }
+    },
+
+    browserify: {
+      dev: {
+        src: ['app/js/**/*.js'],
+        dest: 'build/bundle.js',
+        options: {
+          transform: ['debowerify']
+        }
+      },
+
+      test: {
+        src: ['test/app/client/**/*test.js'],
+        dest: 'test/app/test_bundle.js',
+        options: {
+          transform: ['debowerify']
+        }
+      }
     }
   });
 
   grunt.registerTask('test', ['jshint', 'simplemocha', 'jscs']);
   grunt.registerTask('default', ['test']);
+  grunt.registerTask('build:dev', ['clean:dev', 'browserify:dev', 'copy:dev']);
 };
+
