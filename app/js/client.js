a76416b9286967f1edb10ed68b0e15baba9f1e48
makakoa <makakoa15@gmail.com>
2014-12-06 12:28:30 -0800

    added karma tests

--- app/js/client.js
+++ app/js/client.js
@@ -1,7 +1,23 @@
 'use strict';
 
 require('angular/angular');
+require('angular-route');
 
-var app = angular.module('notesApp', []);
+var notesApp = angular.module('notesApp', ['ngRoute']);
 
-require('./notes/controllers/notesCtrl')(app);
+//services
+require('./notes/services/resource_backend_service')(notesApp);
+
+//controllers
+require('./notes/controllers/notesCtrl')(notesApp);
+
+notesApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+  .when('/notes', {
+    templateUrl: 'templates/notes/notes_template.html',
+    controller: 'notesCtrl'
+  })
+  .otherwise({
+    redirectTo: '/notes'
+  });
+}]);
--- test/app/client/notes_test.js
+++ test/app/client/notes_test.js
@@ -10,7 +10,7 @@ describe('Notes controller', function() {
 
   beforeEach(angular.mock.module('notesApp'));
 
-  beforeEach(angular.mock.inject(function($controller, $rootScope) {
+  beforeEach(angular.mock.inject(function($rootScope, $controller) {
     $scope = $rootScope.$new();
     $controllerConstructor = $controller;
   }));
@@ -19,70 +19,67 @@ describe('Notes controller', function() {
     var notesController = $controllerConstructor('notesCtrl', {$scope: $scope});
     expect(typeof notesController).toBe('object');
   });
+
+  describe('rest requests', function() {
+    beforeEach(angular.mock.inject(function(_$httpBackend_) {
+      $httpBackend = _$httpBackend_;
+      $controllerConstructor('notesCtrl', {$scope: $scope});
+    }));
+
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should make a get request', function() {
+      $httpBackend.expectGET('/api/notes').respond(200, [{'noteBody': 'test note', '_id': '1'}]);
+      $scope.index();
+
+      $httpBackend.flush();
+
+      expect($scope.notes).toBeDefined();
+      expect(Array.isArray($scope.notes)).toBeTruthy();
+      expect(typeof $scope.notes[0]).toBe('object');
+      expect($scope.notes[0].noteBody).toBe('test note');
+    });
+
+    it('should save a new note', function() {
+      $httpBackend.expectPOST('/api/notes').respond(200, {'noteBody': 'test note', '_id': 1});
+      $scope.notes = [];
+      $scope.newNote = {'noteBody': 'test note'};
+      $scope.saveNewNote()
+      $httpBackend.flush();
+
+      expect($scope.notes.length).toBe(1);
+      expect($scope.notes[0].noteBody).toBe('test note');
+      expect($scope.newNote).toBe(null);
+    });
+
+    it('should delete a note', function() {
+      $httpBackend.expectDELETE('/api/notes/1').respond(200);
+      var note= {'noteBody': 'test note', '_id': 1};
+      $scope.notes = [note];
+
+      $scope.deleteNote(note);
+
+      $httpBackend.flush();
+
+      expect($scope.notes.length).toBe(0);
+    });
+
+    it('should edit a note', function() {
+      $httpBackend.expectPUT('/api/notes/1').respond(200);
+
+      var note = {'noteBody': 'test note', '_id':1};
+      $scope.notes = [note];
+      note.noteBody = 'changed test';
+
+      $scope.saveNote(note);
+
+      $httpBackend.flush();
+
+      expect($scope.notes.length).toBe(1);
+      expect($scope.notes[0].noteBody).toBe('changed test');
+    });
+  });
 });
-
-describe('rest requests', angular.mock.inject(function(_$httpBackend_) {
-  beforeEach(angularmock.inject(function(_$httpBackend_) {
-    $httpBackend = _$httpBackend_;
-  }));
-
-  afterEach(function() {
-    $httpBackend.verifyNoOutstandingExpectation();
-    $httpBackend.verifyNoOutstandingRequests();
-  });
-
-  it('should make a get request', function() {
-    $httpBackend.expectGET('/api/notes').respond(200, [{'noteBody': 'test note', '_id': '1'}]);
-
-    var ctrl = $controllerConstructor('notesCtrl', {$scope: $scope});
-    $scope.index();
-
-    $httpBackend.flush();
-
-    expect($scope.notes).toBeDefined();
-    expect(Array.isArray($scope.notes)).toBeTruthy();
-    expect($scope.notes[0]).toBe('object');
-    expect($scope.notes[0].noteBody).toBe('test note');
-  });
-
-  it('can save a note', function() {
-    $httpBackend.expectPOST('/api/notes').respond(200, {'noteBody': 'test note', '_id': 1});
-    $controllerConstructor('notesCtrl', {$scope: $scope});
-    $scope.notes = [];
-    $scope.newNote = {'noteBody': 'test note'};
-    $scope.saveNewNote()
-    $httpBackend.flush();
-
-    expect($scope.notes.length).toBe(1);
-    expect($scope.notes[0].noteBody).toBe('test note');
-    expect($scope.newNote).toBe(null);
-  });
-
-  it('should delete a note', function() {
-    $httpBackend.expectDELETE('/api/notes/1').respond(200);
-    $controllerConstructor('notesCtrl', {$scope: $scope});
-    var note= {'noteBody': 'test note', '_id': 1};
-    $scope.notes = [note];
-
-    $scope.deleteNote(note);
-
-    $httpBackend.flush();
-
-    expect($scope.notes.length).toBe(0);
-  });
-
-  it('should edit a note', function() {
-    $httpBackend.expectPUT('/api/notes/1').respond(200);
-    $controllerConstructor('notesCtrl', {$scope: $scope});
-    var note = {'noteBody': 'test note', '_id':1};
-    $scope.notes = [note];
-    note.noteBody = 'changed test';
-
-    $scope.saveNote(note);
-
-    $httpBackend.flush();
-
-    expect($scope.notes.length).toBe(1);
-    expect($scope.notes[0].noteBody).toBe('test note');
-  });
-}));
